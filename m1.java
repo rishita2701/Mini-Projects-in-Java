1c9822f9b4738561b6d8a0bb70557df8d8edccb4
Rishita Mukherjee <51188766+rishita2701@users.noreply.github.com>
2021-05-10 10:55:46 +0530

    Operators usage

---
+++ m1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class m1 {
+
+	public static void main(String[] args) {
+		
+	
+	Scanner sc=new Scanner(System.in);
+	double x=sc.nextDouble();
+	double valid=x*0.8*0.45;
+	System.out.println(valid);
+	}
+}
